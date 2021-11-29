419dec716e09ccc3f5f947c34ee1be46080619fa
Jarkko Mäenrinta <73038801+jarkmaen@users.noreply.github.com>
2021-11-29 19:22:43 +0200

    yhden tuotteen lisaamisen jalkeen korissa yksi ostosolio

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,24 +4,24 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.ostokset = []
+        self.ostokset_list = []
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self.ostokset)
+        return len(self.ostokset_list)
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         summa = 0
-        for tuote in self.ostokset:
+        for tuote in self.ostokset_list:
             summa += tuote.hinta()
         return summa
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self.ostokset.append(lisattava)
+        self.ostokset_list.append(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -32,6 +32,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self.ostokset_list
